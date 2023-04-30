03c05316f0024f5b4f7e59a5a57c9c1ed5a88ecf
jeb6965 <jordanboykin98@gmail.com>
2023-04-30 19:45:43 -0400

    Capstone project

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
-STATICFILES_DIRS = ( os.path.join('static'), )
+#STATICFILES_DIRS = ( os.path.join('static'), )
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
